2e88a786a34efc721be4180ffd017e8a728279b8
LeandroCostaB <leandro.boscariol@alunos.bpkedu.com.br>
2025-11-16 16:05:55 -0300

    feat: improved filters and added ability to create exams with more than one content

--- lib/models/course_model.dart
+++ lib/models/course_model.dart
@@ -1,18 +1,15 @@
 import 'package:firebase_database/firebase_database.dart';
 
-/// Modelo para `Course` (Curso)
 class Course {
-  final String? id; // Nulável, pois não existe antes de criar
+  final String? id;
   final String name;
 
   Course({this.id, required this.name});
 
-  /// Converte um objeto Dart em um Map para o Realtime Database.
   Map<String, dynamic> toJson() {
     return {'name': name};
   }
 
-  /// Helper para converter um Map genérico (vindo do snapshot) em um Map<String, dynamic>
   static Map<String, dynamic> _dataToMap(DataSnapshot snapshot) {
     final value = snapshot.value;
     if (value is Map) {
@@ -21,9 +18,11 @@ class Course {
     return {};
   }
 
-  /// Cria um objeto Dart a partir de um DataSnapshot do Realtime Database.
   factory Course.fromSnapshot(DataSnapshot snapshot) {
     final data = _dataToMap(snapshot);
-    return Course(id: snapshot.key, name: data['name'] ?? '');
+    return Course(
+      id: snapshot.key,
+      name: data['name']?.toString() ?? ''
+    );
   }
 }
